7bcc9cb84904565b5a858ed6e841eabc2e4097b4
hakimaouini <91877483+hakimaouini@users.noreply.github.com>
2023-08-08 23:44:34 +0200

    creation de base de donnees

---
+++ src/main/java/com/hakim/gestiondestock/model/CommandeFournisseur.java
@@ -0,0 +1,17 @@
+package com.hakim.gestiondestock.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "Commandefournisseur")
+public class CommandeFournisseur extends AbstractEntity{
+}
---
+++ src/main/java/com/hakim/gestiondestock/model/LigneVente.java
@@ -0,0 +1,15 @@
+package com.hakim.gestiondestock.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "lignevente")
+public class LigneVente extends AbstractEntity {
+}
